3b371414a238a6c402aab1417d6e8317eb71788d
Kimura <kimura@tejimaya.com>
2010-10-22 01:38:14 +0900

    generate-allにオプション対応追加。

--- lib/task/opKdtGenerateAllTask.class.php
+++ lib/task/opKdtGenerateAllTask.class.php
@@ -11,8 +11,9 @@ class opKdtGenerateAllTask extends sfBaseTask
 
     $this->addOptions(
       array(
-        new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application', null),
+        new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application', true),
         new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+        new sfCommandOption('size', null, sfCommandOption::PARAMETER_REQUIRED, 'Size of database Pattern', null),
       )
     );
 
@@ -21,11 +22,31 @@ class opKdtGenerateAllTask extends sfBaseTask
 The [opKdt:generate-all|INFO] task generates useless data for testing.
 Call it with:
 
-  [./symfony opKdt:generate-all|INFO]
+  [./symfony opKdt:generate-all --size=small|INFO]
+  size=tiny,small,medium,big,large,huge(config/app.ymlを編集する)
 EOF;
   }
 
   protected function execute($arguments = array(), $options = array())
+  {
+    $tasks = array();
+    if ($options['size'])
+    {
+      $tasks = $this->settask($options['size']);
+    } else {
+      $tasks = $this->setdefaulttask();
+    }
+
+    foreach ($tasks as $task)
+    {
+      $taskName = sprintf('opKdt%sTask', $task['task']);
+      $t = new $taskName($this->dispatcher, $this->formatter);
+      $t->run($arguments = array(),$options = $task['option']);
+      unset($t);
+    }
+  }
+
+  protected function setdefaulttask()
   {
     $tasks = array(
       'GenerateMember',
@@ -38,12 +59,35 @@ EOF;
       'PutFootprint',
       'SendMessage',
     );
-
     foreach ($tasks as $task)
     {
-      $taskName = sprintf('opKdt%sTask', $task);
-      $t = new $taskName($this->dispatcher, $this->formatter);
-      $t->run();
+      $taskoptions[] = array('task' => $task, 'option' => array());
     }
+    return $taskoptions;
   }
+
+  protected function settask($size)
+  {
+    $config = sfConfig::get(sprintf('app_datasize_%s',$size));
+    $type = sfConfig::get(sprintf('app_datatype_%s',$size));
+
+    $taskoptions = array();
+    if ($type =="scenario")
+    {
+      foreach ($config as $num => $task)
+      {
+        foreach ($task as $key => $value)
+        {
+          $taskoptions[] = array('task' => $key, 'option' => $value);
+        }
+      }
+    } else {
+      foreach ($config as $key => $value)
+      {
+        $taskoptions[] = array('task' => $key, 'option' => $value);
+      }
+    }
+    return $taskoptions;
+  }
+
 }
