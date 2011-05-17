7baadf663482e55bcdc7f44216331ef64c255172
nise_nabe <nise.nabe@gmail.com>
2011-05-17 20:28:41 +0900

    add Introductroy essay Generator

---
+++ lib/task/opKdtGenerateIntroFriendTask.class.php
@@ -0,0 +1,40 @@
+<?php
+
+class opKdtGenerateIntroFriendTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace = 'opKdt';
+    $this->name      = 'generate-intro-friend';
+
+    require sfConfig::get('sf_data_dir').'/version.php';
+
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $this->conn = $databaseManager->getDatabase('doctrine')->getDoctrineConnection();
+
+    $sql = 'SELECT id FROM member WHERE is_active != 0';
+    $memberIds = $this->conn->fetchColumn($sql);
+
+    foreach($memberIds as $memberid)
+    {
+      $max = mt_rand(1, count($memberIds));
+      for($i = 0; $i < $max; ++$i)
+      {
+        $from = mt_rand(1, count($memberIds));
+        if($from != $memberid)
+        {
+          $in = new IntroFriend();
+          $in->setMemberIdTo($memberid);
+          $in->setMemberIdFrom($from);
+          $in->setContent('I introduced');
+          $in->save();
+          $this->logSection('write intro ', 'from ' . $from . ' to ' . $memberid);
+        }
+      }
+    }
+  }
+}
