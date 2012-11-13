1c4655851242becab8d493f836f2b2c3b879745d
watanabe <watanabe@openpne.jp>
2012-11-13 15:10:25 +0900

    fixed member generate member form's name

--- apps/pc_backend/lib/form/opKdtPluginBackendMemberForm.class.php
+++ apps/pc_backend/lib/form/opKdtPluginBackendMemberForm.class.php
@@ -8,10 +8,10 @@ class opKdtPluginBackendMemberForm extends BaseForm
   {
     $snsConfigTable = Doctrine::getTable('SnsConfig');
 
-    $this->setWidget($this->generateMember, new sfWidgetFormInput());
-    $this->setValidator($this->generateMember, new sfValidatorInteger(array('min' => 0), array('min' => 'Please input 0 or greater.')));
+    $this->setWidget('number', new sfWidgetFormInput());
+    $this->setValidator('number',  new sfValidatorInteger(array('min' => 0), array('min' => 'Please input 0 or greater.')));
 
-    $this->widgetSchema->setNameFormat('op_kdt_plugin[%s]');
+    $this->widgetSchema->setNameFormat('generate_member[%s]');
   }
 
   public function executeTask($dispatcher)
@@ -22,7 +22,7 @@ class opKdtPluginBackendMemberForm extends BaseForm
     {
       $taskName = sprintf('opKdt%sTask', $this->generateMember);
       $task = new $taskName($dispatcher, new sfFormatter());
-      $task->run($arguments = array(),$options = array('number' => $this->getValue($this->generateMember)));
+      $task->run($arguments = array(), $options = array('number' => $this->getValue($this->generateMember)));
     }
   }
 }
