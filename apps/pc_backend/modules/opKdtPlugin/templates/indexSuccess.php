92f531d912f86ef3761a393835e3c6e78fa4211a
watanabe <watanabe@openpne.jp>
2012-11-13 15:09:58 +0900

    add submenu in backend

--- apps/pc_backend/modules/opKdtPlugin/templates/indexSuccess.php
+++ apps/pc_backend/modules/opKdtPlugin/templates/indexSuccess.php
@@ -1,3 +1,7 @@
-<h2><?php echo __('Kdt Plugin Configuration') ?></h2>
+<?php slot('submenu') ?>
+<?php include_partial('submenu') ?>
+<?php end_slot() ?>
+
+<?php slot('title', __('Kdt Plugin Configuration')) ?>
 
 <?php echo link_to(__('Generate Member'), '@kdt_generate_member') ?>
--- apps/pc_backend/modules/opKdtPlugin/templates/memberSuccess.php
+++ apps/pc_backend/modules/opKdtPlugin/templates/memberSuccess.php
@@ -1,4 +1,8 @@
-<h2><?php echo __('Kdt Plugin Configuration') ?></h2>
+<?php slot('submenu') ?>
+<?php include_partial('submenu') ?>
+<?php end_slot() ?>
+
+<?php slot('title', __('Kdt Plugin Configuration')) ?>
 <h3><?php echo __('Generate Member') ?></h3>
 
 <form action="<?php echo url_for('@kdt_generate_member') ?>" method="POST">
